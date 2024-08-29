2d9ad5cb02cb2ab5ea94534b77d5cc62e1ad9ef4
tonioliii <105586518+tonioliii@users.noreply.github.com>
2024-08-29 19:29:40 +0200

    Add files via upload

--- generate_links.py
+++ generate_links.py
@@ -3,7 +3,7 @@ import clipboard
 txt = "<div id='links'>"
 
 for x in range(10000):
-    txt += f'<a href="file:///C:/Users/antoi/Downloads/classes-main/index.html?id={x}" id="{x}" style="display: none;" target="_blank">Click</a>'
+    txt += f'<a href="https://tonioliii.github.io/classes?id={x}" id="{x}" style="display: none;" target="_blank">Click</a>'
 
 txt += "</div>"
 
